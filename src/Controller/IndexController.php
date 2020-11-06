cc87b6f6d79c6d67ceeb605efa92904a5f70ee92
Daouda KATCHALA MELE <davidkatchala@gmail.com>
2020-11-06 14:02:51 +0100

    amélioration du projet

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -19,7 +19,7 @@ class IndexController extends AbstractController{
     * @Route("/", name="index")
     */
     public function index(){
-        return $this->redirectToRoute('stations');
+        return $this->render('welcome.html.twig', []);
     }
 
     /**
